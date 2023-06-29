10e66e85a9ef9989cbc7adb7f60945cc8dd7883f
tarunwtf <97153927+tarunwtf@users.noreply.github.com>
2023-06-30 00:40:20 +0530

    Tarun's Commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React by tarun
+          Comit by Tarun
         </a>
       </header>
     </div>
