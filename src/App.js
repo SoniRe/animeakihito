90cd07069196b3cb3282b828b66b3b8338dfa76b
Soni <aditya@gmail.com>
2023-06-30 00:50:44 +0530

    Aditya commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Comit by Tarun
+          Comit by aditya
         </a>
       </header>
     </div>
