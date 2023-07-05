795d3dbb7135f481d501dac8f88dc82408376a3e
Soni <aditya@gmail.com>
2023-07-06 00:50:12 +0530

    Safe

--- src/componets/Content/Content.js
+++ src/componets/Content/Content.js
@@ -3,7 +3,9 @@ import Chainsaw from "../../assets/chainsaw-man.jpg";
 
 const Content = () => {
   return (
-    <div className="home">
+    <div className="content">
+      <div className="background" />
+
       <img className="home__image" src={Chainsaw} alt="Chainsaw man photo" />
     </div>
   );
