4f07b055239f3c5bb54ee4486987bb0e2a5a4689
Soni <aditya@gmail.com>
2023-07-08 18:09:24 +0530

    Minor Changes

--- src/componets/Content/Content.js
+++ src/componets/Content/Content.js
@@ -34,7 +34,7 @@ const Content = () => {
           >
             <path d="M17 3H7c-1.1 0-2 .9-2 2v16l7-3l7 3V5c0-1.1-.9-2-2-2zm0 15l-5-2.18L7 18V5h10v13z" />
           </svg>
-          <p className="text">More Info</p>
+          <p className="text">To Watch</p>
         </button>
       </div>
     </div>
